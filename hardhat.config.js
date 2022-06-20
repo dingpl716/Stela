08f3fb58e13980bbbf8bc9717e8cda5c796021df
Peiling Ding <dingpl716@gmail.com>
2022-06-19 21:50:02 -0700

    Use solidity 0.8.4

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require('@nomiclabs/hardhat-ethers');
  */
 module.exports = {
   solidity: {
-    version: "0.8.13",
+    version: "0.8.4",
     settings: {
       optimizer: {
         enabled: true,
